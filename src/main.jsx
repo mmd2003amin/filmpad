6d13ee37a8bf20e87726d1ca397fd9bb3619552f
Amin Basati <mohamadamin386mm@gmail.com>
2024-04-09 11:29:12 +0330

    Connecting the project to the hygraph backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { InMemoryCache, ApolloClient, ApolloProvider } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+  uri:import.meta.env.VITE_HYGRAPH_URI,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
